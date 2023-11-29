511cbab62113155ea0b0ec1949cbe99c8495a064
LuisIriarte5 <luisalejandroiriarte@gmail.com>
2023-11-29 14:23:08 +0100

    Create a function that plays one round of the game

--- script.js
+++ script.js
@@ -10,8 +10,57 @@ function getComputerChoice () {
         case 3:
             return "scissors";
             break;
-        default:
-            return "other?"
-            break;
     }
 }
+
+//The next function plays a single rounf of the game
+function playRound (playerSelection, computerSelection) {
+    console.log(`You selected ${playerSelection} and the computer selected ${computerSelection}, then:`)
+    if (playerSelection === "rock"){
+        switch (computerSelection) {
+            case "rock":
+                return "It's a tie";
+                break;
+            case "papper":
+                return "You lost!";
+                break;
+            case "scissors":
+                return "You won!";
+                break;
+        }
+    }
+    else if (playerSelection === "papper"){
+        switch (computerSelection) {
+            case "rock":
+                return "You won!";
+                break;
+            case "papper":
+                return "It's a tie";
+                break;
+            case "scissors":
+                return "You lost!";
+                break;
+        }
+    }
+    else if (playerSelection === "scissors"){
+        switch (computerSelection) {
+            case "rock":
+                return "You lost!";
+                break;
+            case "papper":
+                return "You won";
+                break;
+            case "scissors":
+                return "It's a tie";
+                break;
+        }
+    }
+}
+
+//Ask for the player's choice with a prompt and store the computer selection
+// const playerSelection = prompt("Write your play! \nRock, papper or scissors?:");
+
+// const computerSelection = getComputerChoice();
+
+
+
